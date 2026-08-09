8da6babbc345bdc3f552bac7d38d6dcf8952faca
akssssss <aksdhama0086@gmail.com>
2026-08-09 12:58:25 +0530

    Rename motion import to _motion

--- components/views/simplifier-view.tsx
+++ components/views/simplifier-view.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { _motion } from "framer-motion";
 import { ArrowRight, Lightbulb, LoaderCircle, Sparkles } from "lucide-react";
 import { useState } from "react";
 
